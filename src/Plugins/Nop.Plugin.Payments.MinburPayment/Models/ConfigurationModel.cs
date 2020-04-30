2ff51018c7a82ccc7a2af41b5d7942139c8d9517
huseyin-altunbas <huseyin.altunbas@tutkal.com.tr>
2020-04-30 18:06:42 +0300

    Payment

--- src/Plugins/Nop.Plugin.Payments.MinburPayment/Models/ConfigurationModel.cs
+++ src/Plugins/Nop.Plugin.Payments.MinburPayment/Models/ConfigurationModel.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using Nop.Web.Framework.Mvc.ModelBinding;
+﻿using Nop.Web.Framework.Mvc.ModelBinding;
 using Nop.Web.Framework.Models;
 
 
@@ -9,17 +8,29 @@ namespace Nop.Plugin.Payments.MinburPayment.Models
     {
         public int ActiveStoreScopeConfiguration { get; set; }
 
-        [NopResourceDisplayName("Plugins.Payments.Manual.Fields.AdditionalFeePercentage")]
-        public bool AdditionalFeePercentage { get; set; }
-        public bool AdditionalFeePercentage_OverrideForStore { get; set; }
+        [NopResourceDisplayName("Plugins.Payments.MinburPayment.Fields.UseSandbox")]
+        public bool UseSandbox { get; set; }
+        public bool UseSandbox_OverrideForStore { get; set; }
 
-        [NopResourceDisplayName("Plugins.Payments.Manual.Fields.AdditionalFee")]
+        [NopResourceDisplayName("Plugins.Payments.MinburPayment.Fields.BusinessEmail")]
+        public string BusinessEmail { get; set; }
+        public bool BusinessEmail_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.MinburPayment.Fields.PDTToken")]
+        public string PdtToken { get; set; }
+        public bool PdtToken_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.MinburPayment.Fields.PassProductNamesAndTotals")]
+        public bool PassProductNamesAndTotals { get; set; }
+        public bool PassProductNamesAndTotals_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.MinburPayment.Fields.AdditionalFee")]
         public decimal AdditionalFee { get; set; }
         public bool AdditionalFee_OverrideForStore { get; set; }
 
-        public int TransactModeId { get; set; }
-        [NopResourceDisplayName("Plugins.Payments.Manual.Fields.TransactMode")]
-        public SelectList TransactModeValues { get; set; }
-        public bool TransactModeId_OverrideForStore { get; set; }
+        [NopResourceDisplayName("Plugins.Payments.MinburPayment.Fields.AdditionalFeePercentage")]
+        public bool AdditionalFeePercentage { get; set; }
+        public bool AdditionalFeePercentage_OverrideForStore { get; set; }
     }
+
 }
